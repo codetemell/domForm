7c4f3d176382714b0fece1f75efb06e93f389ee2
codetemell <temelleldarado@gmail.com>
2023-09-30 07:17:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+// const inputWidth = document.getElementById('inputWidth');
+// const inputHeight = document.getElementById('inputHeight');
+// const inputBackgroundColor = document.getElementById('inputBackgroundColor');
+// const button = document.getElementById('button');
+// const result = document.getElementById('result');
+
+// button.addEventListener('click', (event) => {
+//   event.preventDefault();
+
+//   const width = inputWidth.value;
+//   const height = inputHeight.value;
+//   const backgroundColor = inputBackgroundColor.value;
+
+//   result.style.width = width;
+//   result.style.height = height;
+//   result.style.backgroundColor = backgroundColor;
+// });
+
+// --------------
+// bu calisiyor
+// const input = document.getElementById('input');
+//     const button = document.getElementById('button');
+//     const result = document.getElementById('result');
+
+//     button.addEventListener('click', (event) => {
+//       event.preventDefault();
+
+//       const width = input.value;
+//       const backgroundColor = getRandomColor();
+//       const borderRadius = getRandomBorderRadius();
+
+//       const style = `
+//         width: ${width}px;
+//         background-color: ${backgroundColor};
+//         border-radius: ${borderRadius}px;
+//       `;
+
+//       result.setAttribute('style', style);
+//     });
+//     function getRandomColor() {
+//       const letters = '0123456789ABCDEF';
+//       let color = '#';
+//       for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//       }
+//       return color;
+//     }
+
+//     function getRandomBorderRadius() {
+//       return Math.floor(Math.random() * 50);
+//     }
+
+// 3 yontemm
+// const inputBackgroundColor = document.getElementById('inputBackgroundColor');
+//     const inputBorderRadius = document.getElementById('inputBorderRadius');
+//     const inputWidth = document.getElementById('inputWidth');
+//     const button = document.getElementById('button');
+//     const result = document.getElementById('result');
+
+//     button.addEventListener('click', (event) => {
+//       event.preventDefault();
+
+//       const backgroundColor = inputBackgroundColor.value;
+//       const borderRadius = inputBorderRadius.value;
+//       const width = inputWidth.value;
+
+//       result.style.backgroundColor = backgroundColor;
+//       result.style.borderRadius = borderRadius;
+//       result.style.width = width + 'px';
+//     });
+    // bitti
+    // const inputBackgroundColor = document.getElementById('inputBackgroundColor');
+    // const inputBorderRadius = document.getElementById('inputBorderRadius');
+    // const inputWidth = document.getElementById('inputWidth');
+    // const result = document.getElementById('result');
+
+    // inputBackgroundColor.addEventListener('input', () => {
+    //   result.style.backgroundColor = inputBackgroundColor.value;
+    // });
+
+    // inputBorderRadius.addEventListener('input', () => {
+    //   result.style.borderRadius = inputBorderRadius.value;
+    // });
+
+    // inputWidth.addEventListener('input', () => {
+    //   result.style.width = inputWidth.value + 'px';
+    // });
+
+    // ---------------------------
+
+    // const inputBackgroundColor = document.getElementById('inputBackgroundColor');
+    // const inputBorderRadius = document.getElementById('inputBorderRadius');
+    // const inputWidth = document.getElementById('inputWidth');
+    // const button = document.getElementById('button');
+    // const result = document.getElementById('result');
+
+    // button.addEventListener('click', () => {
+    //   result.style.backgroundColor = inputBackgroundColor.value;
+    //   result.style.borderRadius = inputBorderRadius.value;
+    //   result.style.width = inputWidth.value + 'px';
+    // });
